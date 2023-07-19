00336a5e96b27e5846f52186de3344bb75ecac73
Chris Gray <cgray024@ucr.edu>
2023-07-19 12:06:08 -0700

    Added trapcount pragma to all loops in cpfft_dfi for better diagnosing of bottlenecks.

--- src/cpfft.cpp
+++ src/cpfft.cpp
@@ -66,7 +66,7 @@ extern "C" {void cpfft_dfi(cmplx_type in[N], cmplx_type out[N], cmplx_type twid[
         // uint32_t test_array[32] = {0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 4, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0, 5};
         // uint32_t k = 0;
         outer_loop : for(uint32_t h = 0; h < N; h += 2){
-
+            #pragma HLS loop_tripcount min=512 max=512 
             //generate the binary carry sequence
             uint32_t h2 = h + 2;
             // uint32_t c = 30 - __builtin_clz(h ^ h2);
@@ -97,15 +97,15 @@ extern "C" {void cpfft_dfi(cmplx_type in[N], cmplx_type out[N], cmplx_type twid[
             }
 
             //higher stages
-            inner_loop : for (j; j < 9; j += 2) {
-                if(j < carry[h/2]){
+            inner_loop : for (j; j < carry[h/2]; j += 2) {
+                    #pragma HLS loop_tripcount min=1 max=9 avg=2
                     uint32_t s = 1 << j;
                     uint32_t z = h2 - 4 * s;
                     uint32_t t = log2_n - j - 2;
-
                     //butterfly blocks ------ SOURCE OF CRASH
                     butterfly_loop : for (uint32_t b = 1; b < s / 2; b++) {
                     // w = e^(-2 * M_PI * I * b / s / 4);
+                        #pragma HLS loop_tripcount min=1 max=127 avg=3
                         cmplx_type w = twid[b << t];
                         cmplx_type w_rev;
                         w_rev.real = -1*w.imag;
@@ -124,8 +124,8 @@ extern "C" {void cpfft_dfi(cmplx_type in[N], cmplx_type out[N], cmplx_type twid[
                     spec_case.imag = -1*M_SQRT1_2;
                     //spec_case = (1-i)*(1/sqrt(2))
                     cpfft_bf4(s, out + z + s/2, spec_case);
-                }
             }
+            
 
             /* next input index */
             uint32_t m2 = 0x20000000 >> carry[h/2];
